6e1538e484f8baab70c7c7a886d5b75ed63ef44b
gautam-zemoso <gautam.kumar@zemosolabs.com>
2022-10-07 10:13:44 +0530

    Remove unused files

--- web-ui/src/PaymentReport.js
+++ web-ui/src/PaymentReport.js
@@ -1,7 +1,8 @@
 import './App.css';
 import PaymentReport from '../src/paymentReport.json';
-import clsx from 'clsx';
 import PaymentData from '../src/PaymentJsonData.json';
+import clsx from 'clsx'; 
+
 
 function PaymentPage() {
   const data = PaymentReport;
@@ -30,11 +31,7 @@ function PaymentPage() {
             <p class="text-medium">Download PDF</p>
           </div>
           <div class="report-nav-bar">
-            <p class="text-small">
-              The <strong>Payment Activity Report</strong> provides account activity summary for revenues, expenses, transfers, and tax. Sellers can select custom time period,
-              and download the report in PDF format if needed. Sellers can also download detailed transaction report in .
-              CSV format for revenues, expense, transfers, and tax.
-            </p>
+             <div className={clsx("text-small","report-info")} dangerouslySetInnerHTML={{__html:data["report_info"]}}></div>
             {data?.report_list?.map((step, index) => {
               return <div className="div-list">
                 <div className="circle-1">
@@ -96,12 +93,6 @@ function PaymentPage() {
 
           </div>
         </div>
-        <div className="child-content">
-          <div className={clsx("text-small-01", "text-small-color")}>
-            {data.hub_info}
-          </div>
-        </div>
-
       </div>
     </div>
 
